c1ff1263978f43783633fdb55ad3225d20cda8ab
eslam <i52eslam@gmail.com>
2019-10-27 02:41:16 +0200

    Ternary Expression and code refactorring

---
+++ T0D0/TodoModel.swift
@@ -0,0 +1,18 @@
+//
+//  TodoModel.swift
+//  T0D0
+//
+//  Created by Eslam on 10/27/19.
+//  Copyright © 2019 Eslam. All rights reserved.
+//
+
+import Foundation
+
+class TodoModel {
+    var todoText: String = "x"
+    var isDone: Bool = false
+//    init(_ todoText: String, _ isDone: Bool) {
+//        self.todoText = todoText
+//        self.isDone = isDone
+//    }
+}
--- T0D0/TodoViewController.swift
+++ T0D0/TodoViewController.swift
@@ -3,44 +3,72 @@ import UIKit
 
 class TodoViewController: UITableViewController {
   
-    var list = ["Nasser","Wanda","Ghabbi"]
+    var list: [TodoModel] = []
+    var myTodos = TodoModel()
     let userData = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = self.userData.array(forKey: "Data") as? [String]{
-            self.list = items
-        }
-        
+//        if let items = self.userData.array(forKey: "Data") as? [String]{
+//            self.list = items
+//      }
+       self.myTodos.todoText = "Play"
+        self.myTodos.isDone = true
+        //let new = TodoModel()
+        //new.todoText = "Jaban"
+        self.list.append(myTodos)
     }
-    
+  
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         list.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let thisItem = list[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "todo_cell", for: indexPath)
-        cell.textLabel?.text = list[indexPath.row]
+        cell.textLabel?.text = thisItem.todoText
+        
+        // Ternary Expression
+        // cell.accessoryType = thisItem.isDone == false ? .none : .checkmark
+        
+        // Ternary Expression Shorter
+           cell.accessoryType = thisItem.isDone ? .checkmark : .none
+//         Equilvant to the code above
+        
+/*        if list[indexPath.row].isDone == false {
+            cell.accessoryType = .none
+        } else {
+            cell.accessoryType = .checkmark
+        }
+*/
         return cell
     }
    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if  self.tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            self.tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//      Check True || False
+        list[indexPath.row].isDone = !list[indexPath.row].isDone
+        
+//      Equilvant to the code above
+        
+/*      if list[indexPath.row].isDone == true {
+            list[indexPath.row].isDone = false
         }
         else {
-            self.tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+          list[indexPath.row].isDone = true
         }
-        tableView.deselectRow(at: indexPath, animated: true)
+*/
+        self.tableView.reloadData()
+       tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    // MARK: Add Item Button Bar
     @IBAction func addItem(_ sender: UIBarButtonItem) {
           let ac = UIAlertController(title: "Enter Item", message: nil, preferredStyle: .alert)
           ac.addTextField()
 
           let submitAction = UIAlertAction(title: "Add", style: .default) { [unowned ac] _ in
               let answer = ac.textFields![0]
-              self.list.append(answer.text!)
+            self.list[0].todoText.append(answer.text!)
               self.userData.set(self.list, forKey: "Data")
               self.tableView.reloadData()
           }
